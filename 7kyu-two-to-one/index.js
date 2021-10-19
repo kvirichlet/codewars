ba9685ad205b3676c49f79ad0ee066f540953eab
Vadim Kruglov <vadim.kruglov@kairion.de>
2021-10-19 21:18:03 +0300

    [7kyu] Two to One - adding test case maker

--- 7kyu-two-to-one/index.js
+++ 7kyu-two-to-one/index.js
@@ -1,12 +1,12 @@
 function longest (s1, s2) {
   const letters = {};
   [s1, s2].forEach(s => {
-    for (let char of s.toLowerCase()) {
+    for (const char of s.toLowerCase()) {
       if (!letters[char]) {
         letters[char] = true;
       }
     }
-  })
+  });
   return Object.keys(letters).sort().join('');
 }
 
